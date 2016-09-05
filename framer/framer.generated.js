64e8a3c46a1c3c1f233c72cc063f5aeffa6610b3
Emanuele Salamone <emanuele.salamone@a-tono.com>
2016-09-05 18:18:32 +0200

    Added check and label_success layers

--- framer/framer.generated.js
+++ framer/framer.generated.js
@@ -1214,6 +1214,39 @@ window.__imported__["DropPay Mockups - Android@3x/layers.json.js"] = [
 		"backgroundColor": "rgba(23.969999999999995, 167.79, 239.70000000000002, 1)",
 		"children": [
 			{
+				"objectId": "BEF24850-2B9C-42CB-B565-780AC2149A97",
+				"kind": "text",
+				"name": "vendita3_label_success",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 82,
+					"y": 498,
+					"width": 196,
+					"height": 21
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1,
+					"string": "Pagamento effettuato",
+					"css": [
+						"/* Pagamento effettuato: */",
+						"font-family: Roboto-Medium;",
+						"font-size: 20px;",
+						"color: #FFFFFF;"
+					]
+				},
+				"image": {
+					"path": "images/Layer-vendita3_label_success-qkvgmjq4.png",
+					"frame": {
+						"x": 82,
+						"y": 498,
+						"width": 196,
+						"height": 21
+					}
+				},
+				"children": []
+			},
+			{
 				"objectId": "2E65156A-14DB-4401-8989-117334987AB0",
 				"kind": "text",
 				"name": "vendita3_label_info",
@@ -1277,6 +1310,32 @@ window.__imported__["DropPay Mockups - Android@3x/layers.json.js"] = [
 						"y": 115,
 						"width": 186,
 						"height": 12
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "99911E5E-5C48-4B31-A92B-FEE08F76557C",
+				"kind": "group",
+				"name": "vendita3_check",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 38,
+					"y": 178,
+					"width": 285,
+					"height": 287
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-vendita3_check-otk5mtff.png",
+					"frame": {
+						"x": 38,
+						"y": 178,
+						"width": 285,
+						"height": 287
 					}
 				},
 				"children": []
